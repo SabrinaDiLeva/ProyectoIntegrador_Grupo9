4c4b98f86cb1bdf5a204d718a04504923594e2fd
Marcos Ferro Baya <marcos.ferro@globant.com>
2022-10-26 15:42:23 -0300

    footer terminado

--- src/main/resources/proyecto-integrador/src/components/Bloque-buscador/Buscador.js
+++ src/main/resources/proyecto-integrador/src/components/Bloque-buscador/Buscador.js
@@ -1,35 +1,38 @@
 import React from 'react';
-import './Buscador.css';
+import style from './Buscador.module.css';
 import provincias from './provincias.json';
 
 function Buscador() {
     return (
-        <div className='header-block'>
-            <div className='title'>
-                <h1> Buscar ofertas en hoteles, casas y mucho mas</h1>
+        <>
+            <div className={style.headerBlock}>
+                {/* <span className={style.title}>
+                    
+                </span> */}
+
+                <div className={style.forms1}>
+                    <span className={style.title}>
+                        <h1> Busca ofertas en hoteles, casas y mucho mas...</h1>
+                    </span>
+                    <form className={style.formDondeVamos}>
+                        <label for="exampleSelect"> A donde vamos ?</label>
+                        <select name="exampleSelect" id="exampleSelect">
+                            <option value="" disabled selected>Select an option</option>
+                            {provincias.map((value) => (
+                                <option value={value.key}>{value.nombre}</option>
+                            ))}
+                        </select>
+                    </form>
+
+                    <form className={style.formCheckIn}>
+                        <label for="ckeck">Check in - Check out:</label>
+                        <input type="date" id="ckeck" name="check in-out"></input>
+                    </form>
+
+                    <button className={style.botonBuscar}>Buscar</button>
+                </div>
             </div>
-
-            <div className='forms1'>
-                <form>
-                    <label for="exampleSelect"> A donde vamos ?</label>
-                    <select name="exampleSelect" id="exampleSelect">
-                        <option value="" disabled="disabled" selected="">Select an option</option>
-                       {provincias.map((value) => (
-                        <option value={value.key}>{value.nombre}</option>
-                       ))}
-                    </select>
-                </form>
-                        
-                
-                <form>
-                    <label for="ckeck">Check in - Check out:</label>
-                    <input type="date" id="ckeck" name="check in-out"></input>
-                </form>
-
-                <button>Buscar</button>
-            </div>
-        </div>
-
+        </>
     )
 
 }
--- src/main/resources/proyecto-integrador/src/components/footer/footer.js
+++ src/main/resources/proyecto-integrador/src/components/footer/footer.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro';
-
-import './footer.css';
+import style from './Footer.module.css';
 
 function Footer() {
 
     return (
-        <div className='footer-block'>
-            <div className='footer-block2'>
-                <div className='footer-text'>
-                    <p> ©2022 Digital Booking </p>
+        <>
+        <div className={style.footerBlock}>
+            <div className={style.footerBlock2}>
+                <span className={style.footerText}>
+                    <p>2022 Digital Booking </p>
+                    <small>©</small>
+                </span>
 
-
-                </div>
-
-                    <div className='footer-icons'>
-                        <a href='#'><FontAwesomeIcon icon={brands('twitter')} /></a>
-                        <a href='#'> <FontAwesomeIcon icon={brands('facebook')} /></a>
-                        <a href='#'> <FontAwesomeIcon icon={brands('instagram')} /></a>
-                        <a href='#'><FontAwesomeIcon icon={brands('linkedin')} /></a>
-
-                    </div>
+                <span className={style.footerIcons}>
+                    <a className={style.icono} href='/'><FontAwesomeIcon icon={brands('twitter')}/></a>
+                    <a className={style.icono} href='/'><FontAwesomeIcon icon={brands('facebook')}/></a>
+                    <a className={style.icono} href='/'><FontAwesomeIcon icon={brands('instagram')}/></a>
+                    <a className={style.icono} href='/'><FontAwesomeIcon icon={brands('linkedin')}/></a>
+                </span>
             </div>
         </div>
+        </>
     )
 }
 

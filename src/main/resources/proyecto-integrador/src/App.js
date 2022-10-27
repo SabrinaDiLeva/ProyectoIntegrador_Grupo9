8936f206c608758670d02fafd769745603049e2a
juanco1424 <78766144+juanco1424@users.noreply.github.com>
2022-10-27 12:16:09 -0500

    menu hamburguesa

--- src/main/resources/proyecto-integrador/src/App.js
+++ src/main/resources/proyecto-integrador/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import SeccionCategoria from './components/Categoria/SeccionCategoria';
+import Header from './components/Header/Header';
 import SeccionHeader from './components/Header/SeccionHeader';
 import Login from './components/login/login';
 import Register from './components/Register/register';
@@ -8,7 +9,7 @@ import Register from './components/Register/register';
 function App() {
   return (
     <div className="App">
-      <Register/>
+      <Header/>
     </div>
   );
 }

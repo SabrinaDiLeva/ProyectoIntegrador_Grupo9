ee9efbdbd1f2d6299a9b1c23b795a7f5af78b6b3
ffraga98 <ferfraga2@gmail.com>
2022-10-27 18:27:54 -0300

    Renombre de carpetas

--- src/main/resources/proyecto-integrador/src/App.js
+++ src/main/resources/proyecto-integrador/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import SeccionCategoria from './components/Categoria/SeccionCategoria';
 import SeccionHeader from './components/Header/SeccionHeader';
-import Listado from './components/Listado/Listado.js';
-import Footer from './components/Footer/Footer.js';
+import Listado from './components/Listado/Listado';
+import Footer from './components/Footer/Footer';
 import Buscador from './components/Bloque-buscador/Buscador';
 import Body from './components/Body/Body';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
 function App() {
   return (

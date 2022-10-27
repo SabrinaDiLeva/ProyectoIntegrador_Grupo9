2d7074e46a379ce71edc88414c0710124c4558c9
ffraga98 <ferfraga2@gmail.com>
2022-10-27 12:37:57 -0300

    ADD: Terminados los test de integracion para el CategoriaController

--- src/test/java/com/controller/CategoriaControllerTest.java
+++ src/test/java/com/controller/CategoriaControllerTest.java
@@ -1,9 +1,6 @@
 package com.controller;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,15 +14,48 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
+@TestMethodOrder(MethodOrderer.MethodName.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CategoriaControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    void test01obtenerCategoriaConIdTres() throws Exception {
+    void test01buscarTodasLasCategorias() throws Exception {
+        MvcResult respuesta = this.mockMvc.perform(MockMvcRequestBuilders.get("/categoria/")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print()).andReturn();
+
+        Integer estadoGet = respuesta.getResponse().getStatus();
+        Integer  estadoEsperado = 200;
+
+        Assertions.assertEquals(estadoEsperado, estadoGet);
+
+        String categoria3 = respuesta.getResponse().getContentAsString();
+        String esperado = "[{\"id\":1," +
+                "\"titulo\":\"Hotel\"," +
+                "\"descripcion\":\"Los servicios basicos de un hotel incluyen una cama, un armario y un cuarto de banio. Suelen tener un televisor y una pequenia heladera. Otras instalaciones comunes pueden ser una psicina, un gimnasio o un restaurante.\"," +
+                "\"url_imagen\":\"https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/184305239.jpg?2d22fe63ae1f8960e057238c98fb436f7bd9f65854e3a5e918607c5cfa1d0a52&o=&hp=1\"" +
+                "}";
+        esperado += ",{\"id\":2," +
+                "\"titulo\":\"Hostel\"," +
+                "\"descripcion\":\"Se trata de un albergue juvenil que suele caracterizarse por el bajo precio de las habitaciones, camas o literas y por promover el intercambio cultural y social entre los huespedes.\"," +
+                "\"url_imagen\":\"https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/144249450.jpg?k=125620f12083864434f01ddf9015267929594204e1b0900bb400db48363089e780=\"" +
+                "}";
+        esperado += ",{\"id\":3,\"titulo\":\"Bed and breakfast\",\"descripcion\":\"Un bed and breakfast es un tipo de alojamiento en una residencia privada de pocas habitaciones, que ofrece un desayuno. Se caracterizan por un ambiente familiar y hogarenio, es usual que los duenios de los B&B vivan en el mismo inmueble.\",\"url_imagen\":\"https://hotelpropeller.com/wp-content/uploads/2013/04/Bed-and-Breakfast-Facebook-Page.jpg\"}";
+        esperado += ",{\"id\":4," +
+                "\"titulo\":\"Casa/Departamento\"," +
+                "\"descripcion\":\"Sin descripcion.\"," +
+                "\"url_imagen\":\"https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/184305239.jpg?2d22fe63ae1f8960e057238c98fb436f7bd9f65854e3a5e918607c5cfa1d0a52&o=&hp=1\"" +
+                "}]";
+
+        Assertions.assertEquals(esperado, categoria3);
+
+    }
+    @Test
+    void test02obtenerCategoriaConIdTres() throws Exception {
         MvcResult respuesta = this.mockMvc.perform(MockMvcRequestBuilders.get("/categoria/3")
-                .contentType(MediaType.APPLICATION_JSON))
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andDo(MockMvcResultHandlers.print()).andReturn();
 
         Integer estadoGet = respuesta.getResponse().getStatus();
@@ -39,37 +69,119 @@ class CategoriaControllerTest {
 
     }
     @Test
-    void test02agregarNuevaCategoria() throws Exception {
+    void test03agregarNuevaCategoria() throws Exception {
         String jsonPost = "{\n" +
-                "    \"titulo\" : \"Super Hotel\",\n" +
-                "    \"descripcion\" : \"Nuevo Hotel.\",\n" +
-                "    \"url_imagen\" : \"imagen_test\"\n" +
+                "    \"titulo\" : \"Categoria de prueba\",\n" +
+                "    \"descripcion\" : \"Sin descripcion\",\n" +
+                "    \"url_imagen\" : \"Sin URL\"\n" +
                 "}";
 
         MvcResult respuesta = this.mockMvc.perform(MockMvcRequestBuilders.post("/categoria")
                         .contentType(MediaType.APPLICATION_JSON).content(jsonPost))
                 .andDo(MockMvcResultHandlers.print()).andReturn();
 
-        Integer estadoGet = respuesta.getResponse().getStatus();
+        Integer estadoPost = respuesta.getResponse().getStatus();
         Integer  estadoEsperado = 200;
 
-        Assertions.assertEquals(estadoEsperado, estadoGet);
+        Assertions.assertEquals(estadoEsperado, estadoPost);
 
         respuesta = this.mockMvc.perform(MockMvcRequestBuilders.get("/categoria/5")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andDo(MockMvcResultHandlers.print()).andReturn();
 
-        estadoGet = respuesta.getResponse().getStatus();
+        Integer estadoGet = respuesta.getResponse().getStatus();
         Assertions.assertEquals(estadoEsperado, estadoGet);
 
         String categoria3 = respuesta.getResponse().getContentAsString();
-        String esperado = "{\"id\":5,\"titulo\":\"Super Hotel\",\"descripcion\":\"Nuevo Hotel.\",\"url_imagen\":\"imagen_test\"}";
+        String esperado = "{\"id\":5,\"titulo\":\"Categoria de prueba\",\"descripcion\":\"Sin descripcion\",\"url_imagen\":\"Sin URL\"}";
         Assertions.assertEquals(esperado, categoria3);
     }
-    private void agregarCategoria() throws Exception {
+    @Test
+    void test04eliminarCategoria() throws Exception {
+        MvcResult respuesta = this.mockMvc.perform(MockMvcRequestBuilders.delete("/categoria/5")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print()).andReturn();
+
+
+        Integer estadoDelete = respuesta.getResponse().getStatus();
+        Integer estadoEsperado = 200;
+        Assertions.assertEquals(estadoEsperado, estadoDelete);
+    }
+    @Test
+    void test05obtenerCategoriaEliminada() throws Exception {
+        MvcResult respuesta = this.mockMvc.perform(MockMvcRequestBuilders.get("/categoria/5")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print()).andReturn();
+
+        Integer estadoGet = respuesta.getResponse().getStatus();
+        Integer  estadoEsperado = 404;
+
+        Assertions.assertEquals(estadoEsperado, estadoGet);
+
+        String categoria_eliminada = respuesta.getResponse().getContentAsString();
+        String esperado = "";
+        Assertions.assertEquals(esperado, categoria_eliminada);
+
+    }
+    @Test
+    void test06actualizarCategoria() throws Exception {
+        String jsonPost= "{\n" +
+                "\t\"id\":4,\n" +
+                "\t\"titulo\":\"Casa/Departamento\",\n" +
+                "\t\"descripcion\":\"A definir por el duenio.\",\n" +
+                "\t\"url_imagen\":\"https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/184305239.jpg?2d22fe63ae1f8960e057238c98fb436f7bd9f65854e3a5e918607c5cfa1d0a52&o=&hp=1\"\n" +
+                "}";
+
+        MvcResult respuesta = this.mockMvc.perform(MockMvcRequestBuilders.put("/categoria/4")
+                        .contentType(MediaType.APPLICATION_JSON).content(jsonPost))
+                .andDo(MockMvcResultHandlers.print()).andReturn();
+
+        Integer estadoPut = respuesta.getResponse().getStatus();
+        Integer  estadoEsperado = 200;
+
+        Assertions.assertEquals(estadoEsperado, estadoPut);
+
+        String categoria4 = respuesta.getResponse().getContentAsString();
+        String esperado = "{\"id\":4," +
+                "\"titulo\":\"Casa/Departamento\"," +
+                "\"descripcion\":\"A definir por el duenio.\"," +
+                "\"url_imagen\":\"https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/184305239.jpg?2d22fe63ae1f8960e057238c98fb436f7bd9f65854e3a5e918607c5cfa1d0a52&o=&hp=1\"" +
+                "}";
+
+        Assertions.assertEquals(esperado, categoria4);
+    }
+    @Test
+    void test07actualizarCategoriaInexistente() throws Exception {
+        String jsonPost= "{\n" +
+                "\t\"id\":5,\n" +
+                "\t\"titulo\":\"Casa/Departamento\",\n" +
+                "\t\"descripcion\":\"A definir por el duenio.\",\n" +
+                "\t\"url_imagen\":\"https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/184305239.jpg?2d22fe63ae1f8960e057238c98fb436f7bd9f65854e3a5e918607c5cfa1d0a52&o=&hp=1\"\n" +
+                "}";
+
+        MvcResult respuesta = this.mockMvc.perform(MockMvcRequestBuilders.put("/categoria/5")
+                        .contentType(MediaType.APPLICATION_JSON).content(jsonPost))
+                .andDo(MockMvcResultHandlers.print()).andReturn();
+
+        Integer estadoPut = respuesta.getResponse().getStatus();
+        Integer  estadoEsperado = 404;
+
+        Assertions.assertEquals(estadoEsperado, estadoPut);
+    }
+    @Test
+    void test08eliminarrCategoriaInexistente() throws Exception {
+        MvcResult respuesta = this.mockMvc.perform(MockMvcRequestBuilders.delete("/categoria/5")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print()).andReturn();
+
+        Integer estadoDelete = respuesta.getResponse().getStatus();
+        Integer estadoEsperado = 404;
+        Assertions.assertEquals(estadoEsperado, estadoDelete);
+    }
+    private void agregarTodasLasCategorias() throws Exception {
         String jsonPost = "{\n" +
                 "    \"titulo\" : \"Hotel\",\n" +
-                "    \"descripcion\" : \"Los servicios básicos de un hotel incluyen una cama, un armario y un cuarto de banio. Suelen tener un televisor y una pequenia heladera. Otras instalaciones comunes pueden ser una psicina, un gimnasio o un restaurante.\",\n" +
+                "    \"descripcion\" : \"Los servicios basicos de un hotel incluyen una cama, un armario y un cuarto de banio. Suelen tener un televisor y una pequenia heladera. Otras instalaciones comunes pueden ser una psicina, un gimnasio o un restaurante.\",\n" +
                 "    \"url_imagen\" : \"https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/184305239.jpg?2d22fe63ae1f8960e057238c98fb436f7bd9f65854e3a5e918607c5cfa1d0a52&o=&hp=1\"\n" +
                 "}";
 
@@ -79,7 +191,7 @@ class CategoriaControllerTest {
 
         jsonPost = "{\n" +
                 "    \"titulo\" : \"Hostel\",\n" +
-                "    \"descripcion\" : \"Se trata de un albergue juvenil que suele caracterizarse por el bajo precio de las habitaciones, camas o literas y por promover el intercambio cultural y social entre los huéspedes.\",\n" +
+                "    \"descripcion\" : \"Se trata de un albergue juvenil que suele caracterizarse por el bajo precio de las habitaciones, camas o literas y por promover el intercambio cultural y social entre los huespedes.\",\n" +
                 "    \"url_imagen\" : \"https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/144249450.jpg?k=125620f12083864434f01ddf9015267929594204e1b0900bb400db48363089e780=\"\n" +
                 "}";
 
@@ -110,6 +222,6 @@ class CategoriaControllerTest {
 
     @BeforeAll
     void beforeAllTestAgregarCuatroCategorias() throws Exception {
-        this.agregarCategoria();
+        this.agregarTodasLasCategorias();
     }
 }

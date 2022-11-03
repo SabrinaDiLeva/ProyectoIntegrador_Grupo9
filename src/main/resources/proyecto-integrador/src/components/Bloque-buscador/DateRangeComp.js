32e4acfd18cb5f35b007ed0279c3454d8ab8c118
Marcos Ferro Baya <marcos.ferro@globant.com>
2022-11-03 11:37:53 -0300

    evento listado

--- src/main/resources/proyecto-integrador/src/components/Bloque-buscador/DateRangeComp.js
+++ src/main/resources/proyecto-integrador/src/components/Bloque-buscador/DateRangeComp.js
@@ -62,69 +62,6 @@ const DateRangeComp = () => {
         </div>
     )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-    // const [open, setOpen] = useState(false);
-    // useEffect(() => {
-    //     setCalendar(format(new Date(), 'dd/MM/yyyy'))
-    // }, []);
-
-
-
-//     return (
-//         <>
-//             <div className={style.headerBlock}>
-//                 {/* <span className={style.title}>
-                    
-//                 </span> */}
-
-//                 <div className={style.forms1}>
-//                     <span className={style.title}>
-//                         <h1> Busca ofertas en hoteles, casas y mucho mas...</h1>
-//                     </span>
-//                     <form className={style.formDondeVamos}>
-//                         <label for="exampleSelect"> A donde vamos ?</label>
-//                         <select name="exampleSelect" id="exampleSelect">
-//                             <option value="" disabled selected>Select an option</option>
-//                             {provincias.map((value) => (
-//                                 <option value={value.key}>{value.nombre}</option>
-//                             ))}
-//                         </select>
-//                     </form>
-
-//                     <form className={style.formCheckIn}>
-//                         <label for="ckeck">Check in - Check out:</label>
-//                        <DateRange
-//                         editableDateInputs={true}
-//                         onChange={item => setState([item.selection])}
-//                         moveRangeOnFirstSelection={false}
-//                         rangeColors={['#FBC02D']}
-//                         ranges={state}
-//                         />
-//                         {/* <input type="date" id="ckeck" name="check in-out"></input> */}
-//                     </form>
-
-//                     <button className={style.botonBuscar}>Buscar</button>
-//                 </div>
-//             </div>
-//         </>
-//     )
-
 }
 
 export default DateRangeComp;

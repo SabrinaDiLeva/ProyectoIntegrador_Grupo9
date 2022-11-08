6ac05cb11df8d2f91104e5244133bae33b1374c9
ffraga98 <ferfraga2@gmail.com>
2022-11-08 13:39:49 -0300

    REFACTOR: Dto encargado de validar y cargar la informacion. No el model

--- src/main/java/com/dto/command/CategoriaDTO.java
+++ src/main/java/com/dto/command/CategoriaDTO.java
@@ -1,10 +1,13 @@
 package com.dto.command;
 
 import com.model.Categoria;
+import com.model.Imagen;
 
 import java.io.Serializable;
 
 public class CategoriaDTO implements Serializable {
+    public static final int DESCRIPTION_MAX_LENGTH = 300;
+    public static final int TITULO_MAX_LENGTH = 45;
     private Long id;
     private String titulo;
     private String descripcion;
@@ -15,6 +18,19 @@ public class CategoriaDTO implements Serializable {
         this.titulo = titulo;
         this.descripcion = descripcion;
         this.imagenId = imagenId;
+    }
+    public Categoria newCategoria(Imagen imagen) {
+        return new Categoria( this.id , this.titulo, this.descripcion, imagen);
+    }
+
+    public CategoriaDTO update(Categoria categoria){
+        if (this.titulo!= null && titulo.length() <= TITULO_MAX_LENGTH)
+            categoria.setTitulo(this.titulo);
+        if (this.descripcion != null && descripcion.length() <= DESCRIPTION_MAX_LENGTH)
+            categoria.setDescripcion(this.descripcion);
+        Long id = ( this.imagenId == null )? categoria.getImagen().getId() : this.imagenId;
+
+        return new CategoriaDTO(categoria.getId(), categoria.getTitulo(), categoria.getDescripcion(), id );
     }
 
     public String getTitulo() {

fb250bbc6cad7b01e97562fece39b8e3872235d5
Marcos Ferro <marcossferro@gmail.com>
2022-11-17 19:37:43 -0300

    f

--- Frontend/proyecto-integrador/src/components/Layout/Layout.js
+++ Frontend/proyecto-integrador/src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import {Outlet} from "react-router-dom"
 import SeccionHeader from '../Header/SeccionHeader';
 import Body from '../Body/Body';
-import Footer from "../Footer/footer";
+// import Footer from "../Footer/footer";
 export default function Layout() {
 
     return (
@@ -10,7 +10,7 @@ export default function Layout() {
         <Body>
             <Outlet/>
         </Body>
-        <Footer/>
+        {/* <Footer/> */}
         </>
     )
 }

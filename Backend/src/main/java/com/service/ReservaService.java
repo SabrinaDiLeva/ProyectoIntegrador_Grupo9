9be2b07bc3808b9017ae2f8444f0e6d305d9b09b
unknown <sabridileva@gmail.com>
2022-11-18 19:37:36 -0300

    service nuevos

---
+++ Backend/src/main/java/com/service/ReservaService.java
@@ -0,0 +1,62 @@
+package com.service;
+
+import com.dto.command.ReservaDTO;
+import com.model.Producto;
+import com.model.Reserva;
+import com.model.Usuario;
+import com.repository.IProductoRepository;
+import com.repository.IReservaRepository;
+import com.repository.IUsuarioRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ReservaService implements IService<Reserva,ReservaDTO> {
+    private IReservaRepository iReservaRepository;
+    private IUsuarioRepository iUsuarioRepository;
+    private IProductoRepository iProductoRepository;
+
+    @Autowired
+    public ReservaService(IReservaRepository iReservaRepository, IUsuarioRepository iUsuarioRepository,IProductoRepository iProductoRepository) {
+        this.iReservaRepository = iReservaRepository;
+        this.iUsuarioRepository= iUsuarioRepository;
+        this.iProductoRepository=iProductoRepository;
+    }
+
+    @Override
+    public List<Reserva> listar() {
+        return iReservaRepository.findAll();
+    }
+
+    @Override
+    public Reserva guardar(ReservaDTO reserva) {
+        Usuario usuario = iUsuarioRepository.findById(reserva.getUsuario()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        Producto producto = iProductoRepository.findById(reserva.getProducto()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        Reserva r = reserva.newReserva(usuario,producto);
+        return iReservaRepository.save(r);
+    }
+
+    @Override
+    public Reserva buscar(Long id) {
+        return iReservaRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        this.buscar(id);
+        iReservaRepository.deleteById(id);
+    }
+
+    @Override
+    public Reserva modificar(Long id, ReservaDTO dto) {
+        Reserva reserva = this.buscar(id);
+        return this.guardar(dto.update(reserva));
+    }
+
+}

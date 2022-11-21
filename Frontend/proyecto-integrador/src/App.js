c3f72e2d369dc79ec2ff6d3bd90c0b775b1120a5
Marcos Ferro <marcossferro@gmail.com>
2022-11-21 20:15:05 -0300

    ahora si

--- Frontend/proyecto-integrador/src/App.js
+++ Frontend/proyecto-integrador/src/App.js
@@ -5,8 +5,8 @@ import Listado from './components/Listado/Listado.js';
 import Buscador from './components/Bloque-buscador/Buscador.js';
 import Layout from './components/Layout/Layout.js'
 import Producto from './components/Tarjeta-Producto/Producto.js'
-import Register from './components/registro/asda';
-import Login from './components/sesion/asdasd';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
 
 function App() {
 
--- Frontend/proyecto-integrador/src/components/Layout/Layout.js
+++ Frontend/proyecto-integrador/src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import {Outlet} from "react-router-dom"
 import SeccionHeader from '../Header/SeccionHeader';
 import Body from '../Body/Body';
-import Footer from "../asdasdads/asdad";
+import Footer from "../Footer/Footer";
 
 export default function Layout() {
 

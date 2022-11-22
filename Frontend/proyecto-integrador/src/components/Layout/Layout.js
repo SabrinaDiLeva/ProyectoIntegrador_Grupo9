f756dec8d8e9e01267a1f04bf22f1544f3887c05
Marcos Ferro <marcossferro@gmail.com>
2022-11-22 20:02:26 -0300

    menu + correcciones

--- Frontend/proyecto-integrador/src/components/Layout/Layout.js
+++ Frontend/proyecto-integrador/src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import {Outlet} from "react-router-dom"
 import SeccionHeader from '../Header/SeccionHeader';
 import Body from '../Body/Body';
-import Footer from '../Footer/Footer';
+import Footer from '../elPie/elPie';
 export default function Layout() {
 
     return (

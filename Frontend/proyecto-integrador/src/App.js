e99161820332c52de6971fa2dcda860829476760
Marcos Ferro <marcossferro@gmail.com>
2022-11-22 20:08:44 -0300

    correccion nombre archivos

--- Frontend/proyecto-integrador/src/App.js
+++ Frontend/proyecto-integrador/src/App.js
@@ -5,8 +5,8 @@ import Listado from './components/Listado/Listado';
 import Buscador from './components/Bloque-buscador/Buscador';
 import Layout from './components/Layout/Layout'
 import Producto from './components/Tarjeta-Producto/Producto'
-import Register from './components/Registro/registrop';
-import Login from './components/logueate/logeate';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
 function App() {
 

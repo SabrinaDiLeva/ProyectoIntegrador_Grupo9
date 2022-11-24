9b2db09373864326d2cf6cf67d707312d0223f36
Marcos Ferro <marcossferro@gmail.com>
2022-11-24 10:09:45 -0300

    cambio de url de endpoint

--- Frontend/proyecto-integrador/src/service/ciudadApi.js
+++ Frontend/proyecto-integrador/src/service/ciudadApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:8080/ciudad" // o localhost:8080
+const baseUrl = "http://3.14.41.232:8080/ciudad" // o localhost:8080
 
 export const getCiudades= async() => {
     const endpoint = `${baseUrl}/`
--- Frontend/proyecto-integrador/src/service/productoApi.js
+++ Frontend/proyecto-integrador/src/service/productoApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:8080/producto" // o localhost:8080
+const baseUrl = "http://3.14.41.232:8080/producto" // o localhost:8080
 
 export const getProducto = async (id) => {
     const endpoint = `${baseUrl}/${id}`
